45eba0d8cb97c53af0473f26a585bbb6ab5e56b5
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-09-14 12:30:06 -0400

    array

---
+++ vite-project/recipes.js
@@ -0,0 +1,24 @@
+const meals = [
+  {
+    name: "Pancake",
+    type: "breakfast",
+    ingredients:
+      "1 1/2 cups flour, 2 tp baking powder, 2tbsp sugar, 1/4 tp salt, 1 1/2 milk, 1 egg, 2 tbsp oil",
+    instructions:
+      "Mix dry ingredients, then add milk, oil, and egg. Scoop the batter into medium heated pan. Cook until the top bubbles and flip.",
+  },
+  {
+    name: "French Toast",
+    type: "breakfast",
+    ingredients:
+      "2 slices bread, 1 egg, 1/2 cup milk, 1/4 tp vanilla extract, 1/4 tp sugar, pinch ground cinnamon, 1 tbsp salted butter",
+    instructions:
+      "Whisk egg and milk, then add vanilla, sugar, and cinnamon in a medium-sized bowl. Coat your bread on both sides. Melt butter in the pan and cook slices until golden brown on both sides.",
+  },
+  {
+    name: "Spaghetti",
+    type: "lunch",
+    ingredients: "Ground beef, salt, pepper, oil, seasoning",
+    instructions: "Boil water added with salt. Put half of the spaghetti in.",
+  },
+];
