1b45c1fbcc50aff7a128e86b5280b4236be53600
vvwenv <90654380+vvwenv@users.noreply.github.com>
2022-09-22 12:28:48 -0400

    func

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,21 @@
 import "./style.css";
 import { meals } from "./recipes";
 
-var randomMeal = meals[Math.floor(Math.random() * meals.length)];
-console.log(randomMeal.name);
-console.log(randomMeal.type);
-console.log(randomMeal.ingredients);
-console.log(randomMeal.instructions);
+var rdn = meals[Math.floor(Math.random() * meals.length)];
+console.log(rdn.name);
+console.log(rdn.type);
+console.log(rdn.ingredients);
+console.log(rdn.instructions);
+
+let name = document.querySelector(".name");
+let img = document.querySelector("img");
+let ingredients = document.querySelector(".ingredients");
+let instructions = document.querySelector(".instructions");
+function display() {
+  name.insertAdjacentHTML(
+    "afterbegin",
+    `
+      <h2 class="name">${meals.name[rdn]}</h2>
+      <p class="ingredients">${define.kDefinition}</p>`
+  );
+}
